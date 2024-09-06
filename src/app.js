20eb36440cf3b0c8699e1b627a1e46250592161f
Matias Bangher <matiasbangher06@gmail,com>
2024-09-06 18:23:49 -0300

    inicialiazacion del proy y crud de eventos

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const eventosRoutes = require('./routes/eventos');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); // Para procesar JSON
+app.use('/api/eventos', eventosRoutes); // Ruta para los eventos
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
